8add31324e082825af9534b463e61c73bcf977e3
Juan Pablo Leon <fonseca123papo@gmail.com>
2025-03-13 08:56:02 -0500

    subiedno proyecto api

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import os
 from flask import Flask
 from flask_cors import CORS
 from app.exceptions.exception_globals import handlerExceptionsGlobals
+import gunicorn
+from app.controllers.graph_controllers import graphRoutes
 
 app: Flask = Flask(__name__)
+app.register_blueprint(graphRoutes)
 CORS(app, resources={r"/*": {"origins": "*"}})
 handlerExceptionsGlobals(app=app)
 
+
 if __name__ == '__main__':
-   from app.controllers.graph_controllers import graphRoutes
-   app.register_blueprint(graphRoutes)
-   app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
+   port = os.environ.get("PORT", "10000")
+   os.system(f"gunicorn -b 0.0.0.0:{port} main:app")
